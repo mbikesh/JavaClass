829c6ccbd74166fa6044103ea69a5c37c3ea9bc9
mbikesh <bikeshmaharjan07@gmail.com>
2021-01-05 14:21:34 -0600

    project1

---
+++ src/BankAccount.java
@@ -0,0 +1,94 @@
+public class BankAccount {
+
+    private int balance;
+
+    private int accountNumber;
+
+    private String name;
+
+    private static int accountNum=123456;
+    private int initialBalance;
+    private int previousTransaction;
+
+    public BankAccount(String name){
+        this.name=name;
+        this.initialBalance=0;
+        this.accountNumber=accountNum;
+    }
+    public BankAccount(String name,int balance){
+        this.name=name;
+        this.balance=balance;
+        this.accountNumber=accountNum;
+    }
+
+
+    public void depositFunds(int depositAmount) {
+        if (depositAmount != 0) {
+            this.balance += depositAmount;
+        }
+        previousTransaction=this.balance;
+            displayInfo();
+        System.out.println();
+    }
+    public void depositFunds(int depositAmount,int years){
+        if(depositAmount!=0){
+            this.balance+=depositAmount;
+            previousTransaction=depositAmount;
+        }
+
+        double rate=2;
+        double totalBalance=((rate*this.balance*years)/100)+this.balance;
+        System.out.println("Amount deposited is: $"+depositAmount+" and total balance after " + years+"years is: $"+totalBalance);
+
+      displayInfo();
+        System.out.println();
+    }
+    public void withdraw(int withdrawAmount){
+        if(withdrawAmount!=0){
+            this.balance-=withdrawAmount;
+        }
+        previousTransaction=-withdrawAmount;
+         displayInfo();
+        System.out.println();
+    }
+
+    public void withdraw(int withdrawAmount,int fees){
+        if(withdrawAmount!=0){
+            this.balance=this.balance-(withdrawAmount+fees);
+            previousTransaction=-withdrawAmount;
+        }
+        System.out.println("Withdraw amount is: $"+withdrawAmount+" and withdrawal fee is: -$"+fees);
+      displayInfo();
+        System.out.println();
+    }
+
+
+    public void displayInfo(){
+        System.out.println("Account number is: "+this.accountNumber);
+        System.out.println("Balance is: $"+this.balance);
+
+    }
+    public void previousTransaction(){
+        if(previousTransaction!=0){
+
+            System.out.println("Previous transaction is: "+previousTransaction);
+
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        BankAccount account=new BankAccount("First");
+        account.depositFunds(2000);
+        account.withdraw(23);
+        account.withdraw(54,23);
+        account.depositFunds(400,4);
+        account.previousTransaction();
+        System.out.println();
+
+        BankAccount account1=new BankAccount("Second",2000);
+        account1.displayInfo();
+    }
+}
